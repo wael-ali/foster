31a71193558adc11a6d767a17a8912dce28fbcc5
wael ali <waelali595@gmail.com>
2017-07-16 12:44:22 +0200

    validation in model + controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,27 +10,39 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
   def show
     @place = Place.find(params[:id])
   end
+
   def edit
     @place = Place.find(params[:id])
     if @place.user != current_user
       return render text: 'Not Allowed', status: :forbidden
     end
   end
+
   def update
     @place = Place.find(params[:id])
     if @place.user != current_user
       return render text: 'Editing NOT Allowed !', status: :forbidden
     else 
       @place.update_attributes(place_params)
-      redirect_to @place
+      if @place.valid?
+        redirect_to root_path
+      else
+        render :edit, status: :unprocessable_entity
+      end
     end
   end
+
   def destroy
     @place = Place.find(params[:id])
     if @place.user == current_user
